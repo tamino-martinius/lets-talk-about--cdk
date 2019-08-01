f90c41d6a60f6da7926b73f26f0b1e63fa6e30ac
Tamino Martinius <dev@zaku.eu>
2019-08-01 10:50:33 +0200

    Update demo example

--- demo/1/index.ts
+++ demo/1/index.ts
@@ -1,127 +1,140 @@
-import apigateway = require('@aws-cdk/aws-apigateway'); 
+import apigateway = require('@aws-cdk/aws-apigateway');
 import dynamodb = require('@aws-cdk/aws-dynamodb');
 import lambda = require('@aws-cdk/aws-lambda');
 import cdk = require('@aws-cdk/core');
 
 export class ApiLambdaCrudDynamoDBStack extends cdk.Stack {
+  private defaultPrimaryKey = 'id';
+
   constructor(app: cdk.App, id: string) {
     super(app, id);
 
-    const dynamoTable = new dynamodb.Table(this, 'items', {
+    const shiftsTable = new dynamodb.Table(this, 'shifts', {
       partitionKey: {
-        name: 'itemId',
-        type: dynamodb.AttributeType.STRING
+        name: this.defaultPrimaryKey,
+        type: dynamodb.AttributeType.STRING,
       },
-      tableName: 'items'
+      tableName: 'shifts',
     });
 
     const getOneLambda = new lambda.Function(this, 'getOneItemFunction', {
       code: new lambda.AssetCode('src'),
       handler: 'get-one.handler',
-      runtime: lambda.Runtime.NODEJS_8_10,
+      runtime: lambda.Runtime.NODEJS_10_X,
       environment: {
-        TABLE_NAME: dynamoTable.tableName,
-        PRIMARY_KEY: 'itemId'
-      }
+        TABLE_NAME: shiftsTable.tableName,
+        PRIMARY_KEY: this.defaultPrimaryKey,
+      },
     });
 
     const getAllLambda = new lambda.Function(this, 'getAllItemsFunction', {
       code: new lambda.AssetCode('src'),
       handler: 'get-all.handler',
-      runtime: lambda.Runtime.NODEJS_8_10,
+      runtime: lambda.Runtime.NODEJS_10_X,
       environment: {
-        TABLE_NAME: dynamoTable.tableName,
-        PRIMARY_KEY: 'itemId'
-      }
+        TABLE_NAME: shiftsTable.tableName,
+        PRIMARY_KEY: this.defaultPrimaryKey,
+      },
     });
 
     const createOne = new lambda.Function(this, 'createItemFunction', {
       code: new lambda.AssetCode('src'),
       handler: 'create.handler',
-      runtime: lambda.Runtime.NODEJS_8_10,
+      runtime: lambda.Runtime.NODEJS_10_X,
       environment: {
-        TABLE_NAME: dynamoTable.tableName,
-        PRIMARY_KEY: 'itemId'
-      }
+        TABLE_NAME: shiftsTable.tableName,
+        PRIMARY_KEY: this.defaultPrimaryKey,
+      },
     });
 
     const updateOne = new lambda.Function(this, 'updateItemFunction', {
       code: new lambda.AssetCode('src'),
       handler: 'update-one.handler',
-      runtime: lambda.Runtime.NODEJS_8_10,
+      runtime: lambda.Runtime.NODEJS_10_X,
       environment: {
-        TABLE_NAME: dynamoTable.tableName,
-        PRIMARY_KEY: 'itemId'
-      }
+        TABLE_NAME: shiftsTable.tableName,
+        PRIMARY_KEY: this.defaultPrimaryKey,
+      },
     });
 
     const deleteOne = new lambda.Function(this, 'deleteItemFunction', {
       code: new lambda.AssetCode('src'),
       handler: 'delete-one.handler',
-      runtime: lambda.Runtime.NODEJS_8_10,
+      runtime: lambda.Runtime.NODEJS_10_X,
       environment: {
-        TABLE_NAME: dynamoTable.tableName,
-        PRIMARY_KEY: 'itemId'
-      }
-    });
-    
-    dynamoTable.grantReadWriteData(getAllLambda);
-    dynamoTable.grantReadWriteData(getOneLambda);
-    dynamoTable.grantReadWriteData(createOne);
-    dynamoTable.grantReadWriteData(updateOne);
-    dynamoTable.grantReadWriteData(deleteOne);
-
-    const api = new apigateway.RestApi(this, 'itemsApi', {
-      restApiName: 'Items Service'
+        TABLE_NAME: shiftsTable.tableName,
+        PRIMARY_KEY: this.defaultPrimaryKey,
+      },
     });
 
-    const items = api.root.addResource('items');
+    shiftsTable.grantReadWriteData(getAllLambda);
+    shiftsTable.grantReadWriteData(getOneLambda);
+    shiftsTable.grantReadWriteData(createOne);
+    shiftsTable.grantReadWriteData(updateOne);
+    shiftsTable.grantReadWriteData(deleteOne);
+
+    const api = new apigateway.RestApi(this, 'shiftsApi', {
+      restApiName: 'Shifts Service',
+    });
+
+    const shiftsRoute = api.root.addResource('shifts');
     const getAllIntegration = new apigateway.LambdaIntegration(getAllLambda);
-    items.addMethod('GET', getAllIntegration);
+    shiftsRoute.addMethod('GET', getAllIntegration);
 
     const createOneIntegration = new apigateway.LambdaIntegration(createOne);
-    items.addMethod('POST', createOneIntegration);
-    addCorsOptions(items);
+    shiftsRoute.addMethod('POST', createOneIntegration);
+    this.addCorsOptions(shiftsRoute);
 
-    const singleItem = items.addResource('{id}');
+    const shiftsRouteById = shiftsRoute.addResource('{id}');
     const getOneIntegration = new apigateway.LambdaIntegration(getOneLambda);
-    singleItem.addMethod('GET', getOneIntegration);
+    shiftsRouteById.addMethod('GET', getOneIntegration);
 
     const updateOneIntegration = new apigateway.LambdaIntegration(updateOne);
-    singleItem.addMethod('PATCH', updateOneIntegration);
+    shiftsRouteById.addMethod('PATCH', updateOneIntegration);
 
     const deleteOneIntegration = new apigateway.LambdaIntegration(deleteOne);
-    singleItem.addMethod('DELETE', deleteOneIntegration);
-    addCorsOptions(singleItem);
+    shiftsRouteById.addMethod('DELETE', deleteOneIntegration);
+    this.addCorsOptions(shiftsRouteById);
   }
-}
 
-export function addCorsOptions(apiResource: apigateway.IResource) {
-  apiResource.addMethod('OPTIONS', new apigateway.MockIntegration({
-    integrationResponses: [{
-      statusCode: '200',
-      responseParameters: {
-        'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
-        'method.response.header.Access-Control-Allow-Origin': "'*'",
-        'method.response.header.Access-Control-Allow-Credentials': "'false'",
-        'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET,PUT,POST,DELETE'",
+  private addCorsOptions(apiResource: apigateway.IResource) {
+    apiResource.addMethod(
+      'OPTIONS',
+      new apigateway.MockIntegration({
+        integrationResponses: [
+          {
+            statusCode: '200',
+            responseParameters: {
+              'method.response.header.Access-Control-Allow-Headers':
+                "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
+              'method.response.header.Access-Control-Allow-Origin': "'*'",
+              'method.response.header.Access-Control-Allow-Credentials':
+                "'false'",
+              'method.response.header.Access-Control-Allow-Methods':
+                "'OPTIONS,GET,PUT,POST,DELETE'",
+            },
+          },
+        ],
+        passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+        requestTemplates: {
+          'application/json': '{"statusCode": 200}',
+        },
+      }),
+      {
+        methodResponses: [
+          {
+            statusCode: '200',
+            responseParameters: {
+              'method.response.header.Access-Control-Allow-Headers': true,
+              'method.response.header.Access-Control-Allow-Methods': true,
+              'method.response.header.Access-Control-Allow-Credentials': true,
+              'method.response.header.Access-Control-Allow-Origin': true,
+            },
+          },
+        ],
       },
-    }],
-    passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
-    requestTemplates: {
-      "application/json": "{\"statusCode\": 200}"
-    },
-  }), {
-    methodResponses: [{
-      statusCode: '200',
-      responseParameters: {
-        'method.response.header.Access-Control-Allow-Headers': true,
-        'method.response.header.Access-Control-Allow-Methods': true,
-        'method.response.header.Access-Control-Allow-Credentials': true,
-        'method.response.header.Access-Control-Allow-Origin': true,
-      },  
-    }]
-  })
+    );
+  }
 }
 
 const app = new cdk.App();
