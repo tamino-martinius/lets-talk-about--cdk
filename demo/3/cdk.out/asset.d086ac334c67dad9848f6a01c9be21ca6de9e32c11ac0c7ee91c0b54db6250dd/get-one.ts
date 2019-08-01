ddb9356864039e921b63ae866f38460ff40a0f44
Tamino Martinius <dev@zaku.eu>
2019-08-01 13:46:52 +0200

    Update examples and add demo queries

--- demo/3/cdk.out/asset.d086ac334c67dad9848f6a01c9be21ca6de9e32c11ac0c7ee91c0b54db6250dd/get-one.ts
+++ demo/3/cdk.out/asset.d086ac334c67dad9848f6a01c9be21ca6de9e32c11ac0c7ee91c0b54db6250dd/get-one.ts
@@ -3,25 +3,23 @@ const db = new AWS.DynamoDB.DocumentClient();
 const TABLE_NAME = process.env.TABLE_NAME || '';
 const PRIMARY_KEY = process.env.PRIMARY_KEY || '';
 
-export const handler = async (event: any = {}): Promise<any> => {
+export const handler = async (event: any = {}) : Promise <any> => {
+
   const requestedItemId = event.pathParameters.id;
   if (!requestedItemId) {
-    return {
-      statusCode: 400,
-      body: `Error: You are missing the path parameter id`,
-    };
+    return { statusCode: 400, body: `Error: You are missing the path parameter id` };
   }
 
   const params = {
     TableName: TABLE_NAME,
     Key: {
-      [PRIMARY_KEY]: requestedItemId,
-    },
+      [PRIMARY_KEY]: requestedItemId
+    }
   };
 
   try {
-    await db.delete(params).promise();
-    return { statusCode: 200, body: '' };
+    const response = await db.get(params).promise();
+    return { statusCode: 200, body: JSON.stringify(response.Item) };
   } catch (dbError) {
     return { statusCode: 500, body: JSON.stringify(dbError) };
   }
